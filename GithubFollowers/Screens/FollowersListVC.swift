de976b7f253683536c84741c347a1ae7dbc0d165
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-26 11:09:41 +0100

    Configure flow layout

--- GithubFollowers/Screens/FollowersListVC.swift
+++ GithubFollowers/Screens/FollowersListVC.swift
@@ -13,7 +13,7 @@ class FollowersListVC: UIViewController {
     var collectionView: UICollectionView!
 
     // tip - have viewDidLoad read like a list of functions, refactor all logic out when possible
-	// it's like a table of contents for a book
+    // it's like a table of contents for a book
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
@@ -38,11 +38,31 @@ class FollowersListVC: UIViewController {
         // initialize before adding as subview, as you can't add a nil as a subview
         // view.bounds = fill up whole view
         // collectionViewLayOut: UICollectionViewLayout() is default for now, will customize later
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
         // pink for debugging
         collectionView.backgroundColor = .systemPink
         collectionView.register(GFFollowerCell.self, forCellWithReuseIdentifier: GFFollowerCell.reuseID)
+    }
+
+	// this is going to be refactored out
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        // full width of screen
+        let width = view.bounds.width
+        // UI edge insets
+        let padding: CGFloat = 12
+        // spacing between cells
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+
+        let flowLayout = UICollectionViewFlowLayout()
+        // could refactor to use extension
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        // make height dynamic + some space for the usernameLabel
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+
+        return flowLayout
     }
 
     func getFollowers() {
