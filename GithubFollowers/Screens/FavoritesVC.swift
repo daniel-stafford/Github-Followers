fbbe87a03d460d54f45c191b77b995e7c8fdd952
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-02-09 19:31:15 +0100

    Add favorites table view, custom cell, swipe to delete

--- GithubFollowers/Screens/FavoritesVC.swift
+++ GithubFollowers/Screens/FavoritesVC.swift
@@ -8,18 +8,91 @@
 import UIKit
 
 class FavoritesVC: UIViewController {
+    let tableView = UITableView()
+    var favorites: [Follower] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // system color adjust slightly to dark and light modes
-        view.backgroundColor = .systemBrown
+        configureViewController()
+        configureTableView()
+    }
+
+    // place getFollowers here, as viewDidLoad will not run mid-session
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getFavorites()
+    }
+
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        title = "Favorites"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    func configureTableView() {
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.rowHeight = 80
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        // remember to register the cell! 👀
+        tableView.register(GFFavoriteCell.self, forCellReuseIdentifier: GFFavoriteCell.reuseID)
+    }
+
+    func getFavorites() {
         PersistenceManager.retrieveFavorites { [weak self] result in
-			guard let self = self else { return }
+            guard let self = self else { return }
             switch result {
-            case let .success(followers):
-                print(followers)
+            case let .success(favorites):
+                if favorites.isEmpty {
+                    self.showEmptyStateView(with: "No favorites!\n Add them on the followers screen", in: self.view)
+                } else {
+                    self.favorites = favorites
+                    DispatchQueue.main.async { self.tableView.reloadData() }
+                    // in case emptyState view is being shown
+                    self.view.bringSubviewToFront(self.tableView)
+                }
             case let .failure(error):
-				self.presentGFAlertOnMainThread(alertTitle: "Error", message: error.rawValue, buttonTitle: "OK")
+                self.presentGFAlertOnMainThread(alertTitle: "Error", message: error.rawValue, buttonTitle: "OK")
             }
+        }
+    }
+}
+
+extension FavoritesVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: GFFavoriteCell.reuseID) as? GFFavoriteCell else {
+            fatalError("Unable to get favorite cell")
+        }
+        let favorite = favorites[indexPath.row]
+        cell.set(favorite: favorite)
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let favorite = favorites[indexPath.row]
+        let desVC = FollowersListVC()
+        desVC.username = favorite.login
+        desVC.title = favorite.login
+
+        navigationController?.pushViewController(desVC, animated: true)
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+		let favorite = favorites[indexPath.row]
+		favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+            guard let self = self else { return }
+            guard let error = error else { return }
+            self.presentGFAlertOnMainThread(alertTitle: "Error", message: error.rawValue, buttonTitle: "OK")
         }
     }
 }
